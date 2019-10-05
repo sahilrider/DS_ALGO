e64a31d7d4a3ec9697f621763c55d5c301020740
hosein <hosein.fereydonizad@gmail.com>
2019-10-05 23:47:05 +0330

    sample stack in python.

---
+++ Stacks/stack.py
@@ -0,0 +1,34 @@
+class Stack():
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return len(self.items) == 0
+
+    def length(self):
+        return self.items
+
+    def push(self, element):
+        self.items.append(element)
+
+    def top(self, element, underflow):
+        if self.isEmpty:
+            underflow = True
+        else:
+            underflow = False
+            element = self.items[len(self.items) - 1]
+        return element, underflow
+
+    def pop(self, element, underflow):
+        if self.isEmpty():
+            underflow = True
+
+        else:
+            underflow = False
+            element = self.items.pop()
+        return element, underflow
+
+    def display(self):
+        for i in range(len(self.items)):
+            print(self.items[i])
+
